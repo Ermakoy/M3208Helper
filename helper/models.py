768a90cd89a29d2a950f92ae0061925d13ce9d5f
CheshireB <NikitaUniversity@yandex.ru>
2017-10-02 08:57:38 +0300

    Исправлен link style.css Пример с js

--- helper/models.py
+++ helper/models.py
@@ -3,7 +3,8 @@ from django.db import models
 
 class Folder(models.Model):
     name = models.CharField(max_length=50, verbose_name="Название папки")
-    parent_folder = models.ForeignKey('Folder', blank=True, null=True, related_name='folder')
+
+    parent_folder = models.ForeignKey('Folder', blank=True, null=True, related_name='Child_folder')
 
     def __str__(self):
         return self.name
