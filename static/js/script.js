186ed5ea18ae0e8604352560bc50f43ca6fbfa8b
ermakoy <own3d.rus@gmail.com>
2017-10-02 09:05:35 +0300

    Quick Update со стилями

---
+++ static/js/script.js
@@ -0,0 +1,12 @@
+$("#GetIt").on("click",function getit() {
+    var url_pattern = "http://127.0.0.1:8000/api/get-folder/2";
+
+    $.getJSON(url_pattern, function (data) {
+
+        console.log(data);
+        console.log(data.child_foldres);
+        var question_data = $.parseJSON(data.child_foldres);
+        console.log(question_data);
+        console.log(question_data[0]['fields'])
+    });
+});
