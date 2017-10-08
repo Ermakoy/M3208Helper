c25fef80dffbf891b9d2aeb960e15d3cbbb3683f
ermakoy <own3d.rus@gmail.com>
2017-10-08 17:39:22 +0300

    Перевёрстана страница на grid колонки, добавлена возможность гулять по папкам(пока без возможности вернуться назад) и добавлен задел на обработку шаблонов файлов

--- static/js/script.js
+++ static/js/script.js
@@ -10,23 +10,52 @@ function renderTemplate(name, data) {
     return template;
 }
 
-window.onload = function () {
-    var url_pattern = "http://127.0.0.1:8000/api/get-folder/2";
+$("header").on('click', function () {
+    var url_pattern = "http://127.0.0.1:8000/api/get-root";
 
     $.getJSON(url_pattern, function (data) {
-
-        console.log(data);
-        console.log(data.child_foldres);
-        var question_data = $.parseJSON(data.child_foldres);
-        console.log(question_data);
-        console.log(question_data[0]['fields']);
-        var filesJSON = $.parseJSON(data.child_files);
-        for (i in filesJSON) {
-            html = renderTemplate('files', {
-                name: filesJSON[i]['fields'].name
-            })
-            $('.content').append(html);
+        var foldersJSON = $.parseJSON(data.folder);
+        for (i in foldersJSON) {
+            html = renderTemplate('template-folders', {
+                name: foldersJSON[0]['fields'].name,
+                id: foldersJSON[0].pk
+            });
+            $('.sidebar').append(html);
         }
     });
+});
 
-};
+$('.sidebar').on('click', '.item', function (event) {
+    console.log(event);
+    var element = event.currentTarget;
+    var id = $(element)[0].dataset.id;
+
+    var url_pattern = "http://127.0.0.1:8000/api/get-folder" + "/" + id;
+
+    $.getJSON(url_pattern, function (data) {
+        console.log("OK")
+        console.log(data);
+
+        var child_foldersJSON = $.parseJSON(data.child_folders);
+        var sidebar = $(".sidebar");
+
+        // var filesJSON = $.parseJSON(data.child_files);
+
+        sidebar.empty();
+        console.log(child_foldersJSON);
+
+        for (var i in child_foldersJSON) {
+            html = renderTemplate('template-folders', {
+                name: child_foldersJSON[i]['fields'].name,
+                id: child_foldersJSON[i].pk
+            });
+
+            sidebar.append(html);
+        }
+        //     for (i in filesJSON) {
+        //         html = renderTemplate('files', {
+        //             name: filesJSON[0]['fields'].name
+        //         });
+        //     }
+    });
+});
