f342bc352e03af7486f0cbccd4a9cb9fb20651ee
Andrey <own3d.rus@gmail.com>
2017-12-09 14:56:16 +0300

    Add comments for most functions📄

--- src/helperEvents.js
+++ src/helperEvents.js
@@ -4,14 +4,17 @@ const render = require('./helperRender').render;
 const currentFolder = require('./helperRender').currentFolder;
 
 function init() {
+    // Render the choosen folder
     $('.sidebar').on('click', '.folder', (event) => {
         const element = event.currentTarget;
         const id = $(element)[0].dataset.id;
         render(`http://127.0.0.1:8000/api/get-folder/${id}`);
     });
+    // Going to previous folder
     $('.sidebar').on('click', '.backspace', () => {
         render(`http://127.0.0.1:8000/api/get-folder/${currentFolder().parentFolderID}`);
     });
+    // Creating a new Folder
     $('.appendFolder').on('click', () => {
         const folderName = prompt('Укажите название папки');
         if (folderName !== '' && folderName !== null) {
@@ -27,37 +30,24 @@ function init() {
             });
         }
     });
-    $('.sendFiles').on('click', (event) => {
-        event.preventDefault();
-        let formFiles = $('.files')[0].files;
-        let data = {};
-        data.folder = currentFolder().self.pk;
-        data.files = formFiles;
-        $.ajax({
-            url: 'http://127.0.0.1:8000',
-            type: 'POST',
-            data: data,
-            success() {
-                console.log('success')
-            }
-        })
-    });
+    // Showing FileSend form 
     $('.appendFiles').on('click', () => {
         $('.appendFiles').css('display', 'none');
         $('.appendFiles__form').css('display', 'flex');
     });
+    //Sending files to server
     $('.upload_files').on('click', function (event) {
         let files = $('.files').files;
         event.stopPropagation(); // остановка всех текущих JS событий
         event.preventDefault();  // остановка дефолтного события для текущего элемента - клик для <a> тега
-        if (typeof files == 'undefined') return;
+        if (typeof files === 'undefined') return;
         let data = new FormData();
         $.each(files, function (key, value) {
             data.append(key, value);
         });
         data.append('folder', currentFolder().self.pk);
         $.ajax({
-            url: './submit.php', // TODO Никита, ёбан-бобан поменяй тут URL для запроса
+            url: '', // TODO Никита, ёбан-бобан поменяй тут URL для запроса
             type: 'POST',
             data: data,
             cache: false,
