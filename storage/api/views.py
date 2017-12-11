4309b3d5442a2140c449271bdbf6cd50b5ed152f
Nikita Belayev <NIkitaUniversity@yandex.ru>
2017-12-11 20:14:07 +0300

    Добавлена аунтификация

--- storage/api/views.py
+++ storage/api/views.py
@@ -33,7 +33,7 @@ from storage.models import Folder, File
 class RootFolderAPIView(RetrieveAPIView):
     queryset = Folder.objects.all()
     serializer_class = InFolderSerializer
-    # permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated,)
 
     def get_object(self):
         return self.queryset.get(parent_folder=None)
@@ -43,7 +43,7 @@ class RootFolderAPIView(RetrieveAPIView):
 class FolderAPIView(RetrieveAPIView, UpdateModelMixin, CreateModelMixin, DestroyModelMixin, GenericAPIView):
     queryset = Folder.objects.all()
     serializer_class = InFolderSerializer
-    # permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated,)
 
     def get(self, request, *args, **kwargs):
         return self.retrieve(request, *args, **kwargs)
@@ -54,12 +54,10 @@ class FolderAPIView(RetrieveAPIView, UpdateModelMixin, CreateModelMixin, Destroy
     def put(self, request, *args, **kwargs):
          return self.update(request, *args, **kwargs)
 
-    # def delete(self, request, *args, **kwargs):
-    #     return self.delete(request, *args, **kwargs)
-
 @method_decorator(csrf_exempt, name='dispatch')
 class FileUploadAPIView(APIView):
     parser_classes = (MultiPartParser,)
+    permission_classes = (IsAuthenticated,)
 
     def post(self, request, format=None):
         file_obj_list = request.data.getlist('files')
@@ -71,54 +69,3 @@ class FileUploadAPIView(APIView):
             )
             instance.save()
         return Response(status=204)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# class JSONDetailView(JSONResponseMixin, BaseDetailView):
-#     def get(self, request, *args, **kwargs):
-#         return self.render_to_response(*args, **kwargs)
-#
-#     def render_to_response(self, *args, **response_kwargs):
-#         return self.render_to_json_response(*args, **response_kwargs)
-#
-#
-# class JSONResponseFolderView(JSONDetailView):
-#     def get(self, *args, **kwargs):
-#         try:
-#             folder = Folder.objects.get(id=int(kwargs['id']))
-#         except:
-#             folder = Folder.objects.get(parent_folder=None)
-#
-#         child_folders = Folder.objects.filter(parent_folder=folder.id)
-#         child_files = File.objects.filter(folder=folder)
-#
-#         child_folders_serialize = serializers.serialize('json', child_folders)
-#         child_files_serialize = serializers.serialize('json', child_files)
-#         folder_serialize = serializers.serialize('json', [folder])
-#
-#         data = dict()
-#         data['child_folders'] = child_folders_serialize
-#         data['child_files'] = child_files_serialize
-#         data['folder'] = folder_serialize
-#
-#         return self.render_to_response(*args, **data)
-#
-# @method_decorator(csrf_exempt, name='dispatch')
-# class CreateFolderAPIView(CreateAPIView):
-#     serializer_class = CreateFolderSerializer
-#     permission_classes = (IsAuthenticated,)
