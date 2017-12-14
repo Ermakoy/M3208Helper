c254161ab361de29d1f4e7e2aae44a8c79a6a747
Nikita Belayev <NIkitaUniversity@yandex.ru>
2017-12-14 13:22:57 +0300

    Закомметировано требование аунтификации

--- storage/views.py
+++ storage/views.py
@@ -3,5 +3,5 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.base import TemplateView
 
 
-class IndexView(LoginRequiredMixin, TemplateView):
+class IndexView(TemplateView):
     template_name = 'index.html'
