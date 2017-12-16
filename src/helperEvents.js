fc5f224b831488e0c8ede5d697fce13cd274b6dd
Andrey <own3d.rus@gmail.com>
2017-12-16 14:39:11 +0300

    Huge refactor 🌀 Now all belongs to airbnb standart

--- src/helperEvents.js
+++ src/helperEvents.js
@@ -1,77 +1,84 @@
 // Event functions
-'use strict';
-const render = require('./helperRender').render;
-const currentFolder = require('./helperRender').currentFolder;
-const getCookie = require('./helperRender').getCookie;
+
+const { render, currentFolder, getCookie } = require('./helperRender');
+// const render = require('./helperRender').render;
+// const currentFolder = require('./helperRender').currentFolder;
+// const getCookie = require('./helperRender').getCookie;
 function init() {
-    // Render the choosen folder
-    $('.sidebar').on('click', '.folder', (event) => {
-        const element = event.currentTarget;
-        const id = $(element)[0].dataset.id;
-        render(`http://127.0.0.1:8000/storage/api/folder/${id}/`);
-    });
-    // Going to previous folder
-    $('.sidebar').on('click', '.backspace', () => {
-        render(`http://127.0.0.1:8000/storage/api/folder/${currentFolder().parentFolderID}/`);
-    });
-    // Creating a new Folder
-    $('.appendFolder').on('click', () => {
-        const folderName = prompt('Укажите название папки');
-        if (folderName !== '' && folderName !== null) {
-            $.post({
-                url: 'http://127.0.0.1:8000/storage/api/folder/create/',
-                data: {
-                    name: folderName,
-                    parent_folder: currentFolder().self.pk,
-                },
-                success() {
-                    render(`http://127.0.0.1:8000/storage/api/folder/${currentFolder().self.id}/`);
-                },
-            });
+  // Render the choosen folder
+  $('.sidebar').on('click', '.folder', (event) => {
+    const element = event.currentTarget;
+    const id = $(element)[0].dataset.id;
+    render(`http://127.0.0.1:8000/storage/api/folder/${id}/`);
+  });
+  // Going to previous folder
+  $('.sidebar').on('click', '.backspace', () => {
+    render(`http://127.0.0.1:8000/storage/api/folder/${currentFolder().parentFolderID}/`);
+  });
+  // Creating a new Folder
+  $('.appendFolder').on('click', () => {
+    const folderName = prompt('Укажите название папки');
+    if (folderName !== '' && folderName !== null) {
+      const csrfToken = getCookie('csrftoken');
+      $.ajaxSetup({
+        beforeSend(xhr, settings) {
+          if (!/^(GET|HEAD|OPTIONS|TRACE)$/i.test(settings.type)) {
+            xhr.setRequestHeader('X-CSRFToken', csrfToken);
+          }
+        },
+      });
+      $.post({
+        url: 'http://127.0.0.1:8000/storage/api/folder/create/',
+        data: {
+          name: folderName,
+          parent_folder: currentFolder().self.id,
+        },
+        success() {
+          render(`http://127.0.0.1:8000/storage/api/folder/${currentFolder().self.id}/`);
+        },
+      });
+    }
+  });
+  // Showing FileSend form
+  $('.appendFiles').on('click', () => {
+    $('.appendFiles').css('display', 'none');
+    $('.appendFiles__form').css('display', 'flex');
+  });
+  // Sending files to server
+  $('.sendFiles').on('click', (event) => {
+    event.preventDefault();
+    const files = new FormData();
+    files.append('folder', currentFolder().self.id);
+    files.append('files', $('.files')[0].files);
+    const csrfToken = getCookie('csrftoken');
+    $.ajaxSetup({
+      beforeSend(xhr, settings) {
+        if (!/^(GET|HEAD|OPTIONS|TRACE)$/i.test(settings.type)) {
+          xhr.setRequestHeader('X-CSRFToken', csrfToken);
         }
+      },
     });
-    // Showing FileSend form 
-    $('.appendFiles').on('click', () => {
-        $('.appendFiles').css('display', 'none');
-        $('.appendFiles__form').css('display', 'flex');
-    });
-    //Sending files to server
-    $('.sendFiles').on('click', function (event) {
-        event.preventDefault();
-        let files = new FormData();
-        files.append('folder', currentFolder().self.id);
-        files.append('files', $('.files')[0].files);
-        let csrfToken = getCookie('csrftoken');
-        $.ajaxSetup({
-            beforeSend: function (xhr, settings) {
-                if (!/^(GET|HEAD|OPTIONS|TRACE)$/i.test(settings.type)) {
-                    xhr.setRequestHeader("X-CSRFToken", csrfToken);
-                }
-            }
-        });
-        $.ajax({
-            url: 'http://127.0.0.1:8000/storage/api/upload/',
-            data: files,
-            method: 'POST',
-            processData: false,
-            contentType: false,
-            success: function (respond, status, jqXHR) {
-                if (typeof respond.error === 'undefined') {
-                    console.log('Всё прошло нормально')
-                }
-                else {
-                    console.log('КОД КРАСНЫЙ КОД КРАСНЫЙ');
-                }
-            },
-            error: function (jqXHR, status, errorThrown) {
-                console.log('ОШИБКА AJAX запроса: ' + status, jqXHR);
-            }
-
-        });
+    $.ajax({
+      url: 'http://127.0.0.1:8000/storage/api/upload/',
+      data: files,
+      method: 'POST',
+      processData: false,
+      contentType: false,
+      success(respond) {
+        if (typeof respond.error === 'undefined') {
+          console.log('Всё прошло нормально');
+        } else {
+          console.log('КОД КРАСНЫЙ КОД КРАСНЫЙ');
+        }
+      },
+      error(jqXHR, status) {
+        console.log(`ОШИБКА AJAX запроса: ${status}`, jqXHR);
+      },
 
     });
+  });
 }
 
 module.exports = {
-    init,
+  init,
 };
--- src/helperRender.js
+++ src/helperRender.js
@@ -1,149 +1,163 @@
 /* eslint-disable max-len */
-'use strict';
+
+
 // Render and technician function
 const FolderClass = require('./helperFolder');
 
 let currentFolder;
 
 function setBackspace() {
-    $('.sidebar').prepend('<div class=\'backspace\'>\n' +
+  $('.sidebar').prepend('<div class=\'backspace\'>\n' +
         '            <div class=\'folderName\'>Вернуться назад</div>\n' +
         '        </div>');
 }
 /**
  * Function, that renders Folder and File template
- * 
+ *
  * @param {string} name name of the template
  * @param {object} data object with data to print
  * @returns DOM node
  */
 function renderTemplate(name, data) {
-    let template = document.getElementById(name).innerHTML;
+  let template = document.getElementById(name).innerHTML;
 
-    for (const property in data) {
-        if (data.hasOwnProperty(property)) {
-            const search = new RegExp(`{${property}}`, 'g');
-            template = template.replace(search, data[property]);
-        }
+  for (const property in data) {
+    if (Object.prototype.hasOwnProperty.call(data, property)) {
+      const search = new RegExp(`{${property}}`, 'g');
+      template = template.replace(search, data[property]);
     }
-    return template;
+  }
+  return template;
 }
 /**
  * Function returns part of Cookie
- * 
- * @param {string} name 
- * @returns 
+ *
+ * @param {string} name
+ * @returns
  */
 function getCookie(name) {
-    let cookieValue = null;
-    if (document.cookie && document.cookie != '') {
-        let cookies = document.cookie.split(';');
-        for (let i = 0; i < cookies.length; i++) {
-            let cookie = jQuery.trim(cookies[i]);
-            // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) == (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
+  let cookieValue = null;
+  if (document.cookie && document.cookie !== '') {
+    const cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i += 1) {
+      const cookie = jQuery.trim(cookies[i]);
+      // Does this cookie string begin with the name we want?
+      if (cookie.substring(0, name.length + 1) === (`${name}=`)) {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
+      }
     }
-    return cookieValue;
+  }
+  return cookieValue;
 }
 /**
  * Function checking the extension of file, to define a file icon
- * 
+ *
  * @param {string} file Filename string
  * @returns Extension of file or -1 if it hasn't
  */
-function checkExtension(file) {
-    file=file.substring(file.lastIndexOf('.')+1);
-    if (file !== null) {
-        return file;
-    }else{
-        return -1;
-    }
-}
+// function checkExtension(fileString) {
+//   const file = fileString.substring(fileString.lastIndexOf('.') + 1);
+//   if (file !== null) {
+//     return file;
+//   }
+//   return -1;
+// }
 /**
  * Function for getting parameters from URL's Querystring
- * 
+ *
  * @param {string} name Name for searching parameter
- * @param {string} url 
+ * @param {string} url
  * @returns Value of parameter
  */
-function getParameterByName(name, url) {
-    if (!url) url = window.location.href;
-    name = name.replace(/[\[\]]/g, '\\$&');
-    const regex = new RegExp(`[?&]${name}(=([^&#]*)|&|#|$)`);
-    const results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+function getParameterByName(nameString) {
+  const urlString = window.location.href;
+  // eslint-disable-next-line
+  const name = nameString.replace(/[\[\]]/g, '\\$&');
+  const regex = new RegExp(`[?&]${name}(=([^&#]*)|&|#|$)`);
+  const results = regex.exec(urlString);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+/**
+ * Callback function that renders all subfolders in this folders
+ *
+ * @param {Object} folder
+ */
+function renderFolders(folder) {
+  const html = renderTemplate('template-folders', {
+    name: folder.name,
+    id: folder.id,
+  });
+  $('.sidebar__content').append(html);
+}
+/**
+ * Callback function, that renders all files in this folder
+ *
+ * @param {Object} file
+ */
+function renderFiles(file) {
+  let fileName = file.name;
+  if (fileName.indexOf('.') !== -1) {
+    fileName = fileName.substring(0, fileName.indexOf('.'));
+  }
+  const html = renderTemplate('files', {
+    name: fileName,
+    link: `http://127.0.0.1:8000/media/${file.file}`,
+    date: file.date_creation,
+  });
+  $('.content').append(html);
 }
 /**
  * This function renders folders and files by taking a urlPattern string
- * 
- * @param {string} urlPattern 
+ *
+ * @param {string} urlPattern
  */
 function render(urlPattern) {
-    $.getJSON(urlPattern, (data) => {
-        currentFolder = new FolderClass(data.child_folders, data.files,{id : data.id, name: data.name, parentID : data.parent_folder});
-        const sidebarContent = $('.sidebar__content');
-        const content = $('.content');
-        sidebarContent.empty();
-        content.empty();
-        for (const folder of currentFolder.childFolders) {
-            const html = renderTemplate('template-folders', {
-                name: folder.name,
-                id: folder.id,
-            });
-            sidebarContent.append(html);
-        }
-        for (const file of currentFolder.files) {
-            let fileName = file.name;
-            if (fileName.indexOf('.') !== -1) {
-                fileName = fileName.substring(0, fileName.indexOf('.'));
-            }
-            const html = renderTemplate('files', {
-                name: fileName,
-                link: `http://127.0.0.1:8000/media/${file.file}`,
-                date: file.date_creation,
-            });
-            content.append(html);
-        }
-        if (currentFolder.parentFolderID === null) {
-            $('.backspace').css('display', 'none');
-        } else if ($('.backspace').length) {
-            $('.backspace').css('display', 'flex');
-        } else {
-            setBackspace();
-        }
-        $('.appendFiles__form').css('display','none');
-        $('.appendFiles').css('display','flex');
-        history.pushState(null, null, `?folder='${currentFolder.self.id}'`);
-    });
+  $.getJSON(urlPattern, (data) => {
+    currentFolder = new FolderClass(data.child_folders, data.files, { id: data.id, name: data.name, parentID: data.parent_folder });
+    const sidebarContent = $('.sidebar__content');
+    sidebarContent.empty();
+    const content = $('.content');
+    content.empty();
+    currentFolder.childFolders.forEach(renderFolders);
+    currentFolder.files.forEach(renderFiles);
+    if (currentFolder.parentFolderID === null) {
+      $('.backspace').css('display', 'none');
+    } else if ($('.backspace').length) {
+      $('.backspace').css('display', 'flex');
+    } else {
+      setBackspace();
+    }
+    $('.appendFiles__form').css('display', 'none');
+    $('.appendFiles').css('display', 'flex');
+    // eslint-disable-next-line
+    history.pushState(null, null, `?folder='${currentFolder.self.id}'`);
+  });
 }
 
 function init() {
-    const folderParam = getParameterByName('folder');
-    const fileParam = getParameterByName('file');
-    if (folderParam === null) {
-        let urlPattern = 'http://127.0.0.1:8000/storage/api/root/';
-        $.getJSON(urlPattern, (data) => {
-            currentFolder = new FolderClass(null,data.files, {id : data.id, name : data.name, parentID : data.parent_folder});
-            urlPattern = `http://127.0.0.1:8000/storage/api/folder/${currentFolder.self.id}/`;
-            setBackspace();
-        });
-        render(urlPattern);
-    } else if (fileParam === null) {
-        render(`http://127.0.0.1:8000/storage/api/folder/${folderParam.substr(1, folderParam.length - 2)}/`);
-    }
+  const folderParam = getParameterByName('folder');
+  const fileParam = getParameterByName('file');
+  if (folderParam === null) {
+    let urlPattern = 'http://127.0.0.1:8000/storage/api/root/';
+    $.getJSON(urlPattern, (data) => {
+      currentFolder = new FolderClass(null, data.files, { id: data.id, name: data.name, parentID: data.parent_folder });
+      urlPattern = `http://127.0.0.1:8000/storage/api/folder/${currentFolder.self.id}/`;
+      setBackspace();
+    });
+    render(urlPattern);
+  } else if (fileParam === null) {
+    render(`http://127.0.0.1:8000/storage/api/folder/${folderParam.substr(1, folderParam.length - 2)}/`);
+  }
 }
 
 module.exports = {
-    currentFolder() {
-        return currentFolder;
-    },
-    init,
-    render,
-    getCookie
+  currentFolder() {
+    return currentFolder;
+  },
+  init,
+  render,
+  getCookie,
 };
